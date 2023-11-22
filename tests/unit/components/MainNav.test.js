71aabdd3e18b97636b808a2dda54e1819dd01ac5
Chris Gartsen <chris@gartsen.nl>
2023-11-22 22:00:02 +0100

    Add navigation menu items

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,4 +7,14 @@ describe('MainNav', () => {
     const companyName = screen.getByText('Vue Careers')
     expect(companyName).toBeInTheDocument()
   })
+
+  it('displays menu items for navigation', () => {
+    const menuItems = ['Teams', 'Locations', 'Life a Vue Corp', 'How we hire', 'Students', 'Jobs']
+
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent)
+
+    expect(navigationMenuTexts).toEqual(menuItems)
+  })
 })
